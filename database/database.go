b3eff06818b987d5a415c2259401fd3341044ca9
AdeleRICHARD <adrichard@figarocms.fr>
2024-07-19 17:45:59 +0200

    Finish created first base db

--- database/database.go
+++ database/database.go
@@ -35,32 +35,32 @@ func NewDB(path string) (*DB, error) {
 		return nil, err
 	}
 
-	log.Println("New database created")
-
 	return db, nil
 }
 
 // CreateChirp creates a new chirp and saves it to disk
 func (db *DB) CreateChirp(body string) (*Chirp, error) {
-	db.mux.Lock()
-	defer db.mux.Unlock()
-
 	var newChirp Chirp
 	dbStructure, err := db.loadDB()
 	if errors.Is(err, ErrDBNotCreated) {
 		return nil, err
 	}
 
+	println(len(dbStructure.Chirps))
+
 	if len(dbStructure.Chirps) > 0 {
-		newChirp.ID = dbStructure.Chirps[len(dbStructure.Chirps)-1].ID + 1
-		newChirp.Body = body
-
-		return &newChirp, nil
+		// Find the highest ID
+		maxID := 0
+		for _, chirp := range dbStructure.Chirps {
+			if chirp.ID > maxID {
+				maxID = chirp.ID
+			}
+		}
+		newChirp.ID = maxID + 1
+	} else {
+		newChirp.ID = 1
 	}
-
-	// create New chirp
 	newChirp.Body = body
-	newChirp.ID = 1
 
 	// Add chirp to db structure
 	dbStructure.Chirps[newChirp.ID] = newChirp
@@ -69,27 +69,31 @@ func (db *DB) CreateChirp(body string) (*Chirp, error) {
 		return nil, err
 	}
 
+	log.Println("Chirp created")
+
 	return &newChirp, nil
 
 }
 
 // GetChirps returns all chirps in the database
-func (db *DB) GetChirp() ([]Chirp, error) {
-	if err := db.ensureDB(); err != nil {
-		return nil, err
-	}
+func (db *DB) GetChirp() (map[int]Chirp, error) {
+	db.mux.RLocker()
+	defer db.mux.RUnlock()
 
-	_, err := db.loadDB()
+	dbStructure, err := db.loadDB()
 	if err != nil {
 		return nil, err
 	}
 
-	return nil, nil
+	return dbStructure.Chirps, nil
 
 }
 
 // ensureDB creates a new database file if it doesn't exist
 func (db *DB) ensureDB() error {
+	db.mux.Lock()
+	defer db.mux.Unlock()
+
 	if _, err := os.Stat(db.path); os.IsNotExist(err) {
 		// File does not exist, create it
 		initialDB := DBStructure{Chirps: make(map[int]Chirp)}
@@ -98,6 +102,7 @@ func (db *DB) ensureDB() error {
 			return ErrDBNotCreated
 		}
 		if err := os.WriteFile(db.path, data, 0644); err != nil {
+			log.Println("New database created")
 			println(err.Error())
 			return ErrDBNotCreated
 		}
@@ -108,10 +113,35 @@ func (db *DB) ensureDB() error {
 
 // loadDB reads the database file into memory
 func (db *DB) loadDB() (DBStructure, error) {
-	return DBStructure{}, nil
+	db.mux.RLock()
+	defer db.mux.RUnlock()
+
+	log.Println("Loading database")
+	file, err := os.ReadFile(db.path)
+	if err != nil {
+		return DBStructure{}, err
+	}
+
+	var dbStructure DBStructure
+	err = json.Unmarshal(file, &dbStructure)
+	if err != nil {
+		return DBStructure{}, err
+	}
+
+	return dbStructure, nil
 }
 
 // writeDB writes the database file to disk
-func (wb *DB) writeDB(dbStructure DBStructure) error {
+func (db *DB) writeDB(dbStructure DBStructure) error {
+	data, err := json.Marshal(dbStructure)
+	if err != nil {
+		return err
+	}
+
+	log.Println("Writing to database")
+	if err := os.WriteFile(db.path, data, 0644); err != nil {
+		return err
+	}
+
 	return nil
 }
